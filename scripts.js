7db0fef20914b9f7de6fb1dd0f1a0388670ad267
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2023-01-23 16:54:33 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
  $(function(){
-		$('input.purple_button[type="submit"]').click(function(){
-			$('#success_message').html('Aguarde um instante...').show();
-				var fields = $('form').serialize();
-				$.ajax({
-					url: 'http://servicemail/',
-					dataType: 'json',
-					type:'POST',
-					data: fields,
-					success: function( data ) {			
-						$('#success_message').html('Sua mensagem foi enviada com sucesso!').show();
-					}
-				});
-			$('#contact_form').hide();
-			return false;    
-		});  
-	})
+	$('input.purple_button[type="submit"]').click(function(){
+		$('#success_message').html('Aguarde um instante...').show();
+		var fields = $('form').serialize();
+		$.ajax({
+			url: 'http://servicemail/',
+			dataType: 'json',
+			type:'POST',
+			data: fields,
+			success: function( data ) {			
+				$('#success_message').html('Sua mensagem foi enviada com sucesso!').show();
+			}
+		});
+		$('#contact_form').hide();
+		return false;    
+	});  
+})
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-56932919-5', 'auto');
